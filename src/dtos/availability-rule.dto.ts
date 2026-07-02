ae075cd1d3ff748fb0ac974eca8a05e8e5222c46
benil09 <benil8235@gmail.com>
2026-07-02 22:45:45 +0530

    Added custom zod validator using refine and super refine

--- src/dtos/availability-rule.dto.ts
+++ src/dtos/availability-rule.dto.ts
@@ -8,25 +8,53 @@ export const createAvailabilityRuleBaseSchema = z.object({
     weekday:z.number().int().min(0).max(6),
     startTime:z.string().regex(timeRegex,"invalid time format HH:MM"),
     endTime:z.string().regex(timeRegex,"invalid time format HH:MM"),
-    isActive:z.boolean(),
+    isActive:z.boolean().default(true),
     timezone:z.string().default("UTC"),
 })
     
+// Problem -> Anybody could come and set the start time which is greater than the end time 
+// this is not ideal so we need to fix this -> using .refine() function 
+
+export const createAvailabilityRuleSchema = createAvailabilityRuleBaseSchema.refine((rule)=>{
+    return rule.startTime <= rule.endTime || "End time must be after start time"
+})
+
+
 
 export const updateAvailabilityRuleBaseSchema = createAvailabilityRuleBaseSchema.partial();
 
-export const createAvailabilityExceptionSchema = z.object({
+export const createAvailabilityExceptionBaseSchema = z.object({
     date:z.string().regex(dateRegex,"invalid date format YYYY-MM-DD"),
     type: z.enum(["BLOCK_FULL_DAY", "BLOCK_PARTIAL", "ADD_AVAILABLE_WINDOW"]),
     startTime:z.string().regex(timeRegex,"invalid time format HH:MM").optional(),
     endTime:z.string().regex(timeRegex,"invalid time format HH:MM").optional(),
     timezone:z.string().default("UTC"),
     reason:z.string().max(500).optional()
+}).superRefine((data,ctx)=>{
+    if(data.type !== "BLOCK_FULL_DAY"){
+        if(!data.startTime || !data.endTime){
+            ctx.addIssue({
+                code:"custom",
+                message:"Start time and end time are required for partial/add types",
+                path:["startTime","endTime"]
+            })
+        } else{
+            if(data.startTime >= data.endTime){
+                ctx.addIssue({
+                    code:"custom",
+                    message:"End time must be after start time",
+                    path:["endTime"]
+                })
+            }
+        }
+    }
 })
 
-export const updateExceptionSchema = createAvailabilityExceptionSchema.partial();
+
+
+export const updateExceptionSchema = createAvailabilityExceptionBaseSchema.partial();
 
 export type CreateAvailabilityRuleDto = z.infer<typeof createAvailabilityRuleBaseSchema>;
 export type UpdateAvailabilityRuleDto = z.infer<typeof updateAvailabilityRuleBaseSchema>;
-export type createAvailabilityExceptionDto = z.infer<typeof createAvailabilityExceptionSchema>;
+export type createAvailabilityExceptionDto = z.infer<typeof createAvailabilityExceptionBaseSchema>;
 export type UpdateExceptionDto = z.infer<typeof updateExceptionSchema>;
